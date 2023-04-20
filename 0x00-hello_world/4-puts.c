bebda4916aa469e5990c8f219543974be748927d
Jordan <measlybean@gmail.com>
2023-04-20 06:04:03 -0700

    Changes to file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
-int main(){
-  puts("Programming is like building a multilingual puzzle");
-  return 0;
+/**
+* main -  prints a string
+* function takes no paramters
+*
+* A string is printed to the standard output
+* Return: zero
+*/
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
 }
