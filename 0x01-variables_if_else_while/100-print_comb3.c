e8e59a02e780f7dd98f36cd673a6652af21b209a
Jordan <measlybean@gmail.com>
2023-04-21 14:28:13 -0700

    Added file 100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - Entry point
+*
+* prints numbers to stdout
+* Return: returns 0
+*/
+int main(void)
+{
+	/* define */
+	int i;
+	int a;
+
+	/* init */
+	i = 01;
+	a = i;
+
+	while (i < 90)
+	{
+		putchar((i % 10 + '0'));
+		if ((i - 9) % 10 == 0)
+		{
+			a = a + 11;
+			i = a;
+			continue;
+		}
+
+		if (i < 89)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		i++;
+	}
+	return (0);
+}
