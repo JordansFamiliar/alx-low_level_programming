1372a6a54d7700b60f2db0236a91af02c8d031e5
Jordan <measlybean@gmail.com>
2023-04-21 08:09:44 -0700

    Changes to files

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -11,15 +11,17 @@ int main(void)
 {
 	/*define variables*/
 	int i;
+	char n;
 
 	/*initialise*/
 	i = 0;
+	char = '\n';
 
 	while (i < 9)
 	{
 		printf("%d", i);
 		if (i == 9)
-			printf('\n');
+			printf("%d", n);
 		i++;
 	}
 
--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -13,7 +13,7 @@ int main(void)
 	int i;
 
 	/* initialise */
-	int i = 0;
+	i = 0;
 
 	while (i < 9)
 	{
