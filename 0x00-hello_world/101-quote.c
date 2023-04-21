338cba24062666a7c5d61adcc443e449df92da3f
Jordan <measlybean@gmail.com>
2023-04-21 01:22:47 -0700

    Added files

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main - prints a string
+* main has no variables
+*
+* prints a string to stderr
+* Return: 1
+*/
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar
+, 2015-10-19\n");
+	return (1);
+}
