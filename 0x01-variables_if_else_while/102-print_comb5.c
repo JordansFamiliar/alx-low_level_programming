78d2d337f8bca360d7ad873babd7ad9c01bd8643
Jordan <measlybean@gmail.com>
2023-04-24 14:54:40 -0700

    Added file 102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - entry point
+*
+* prints to stdout
+*Return: 0 Success
+*/
+int main(void)
+{
+	int a, b, c, d, e, x;
+
+	for (a = 0; a < 10; a++)
+	{
+		for (b = 0; b < 10; b++)
+		{
+			for (c = 0; b < 10; c++)
+			{
+				for (d = 0; d < 10; d++)
+				{
+					for (e = 1, e < 5; e++)
+					{
+						if (e == 3)
+							x = c;
+						if (e == 4)
+						{
+							x = d;
+							e = 1;
+							putchar(x % 10 + '0');
+							if (a == 9 && b == 8 &&
+							    c == 9 && c == 9)
+								break;
+							putchar(',');
+							putchar(' ');
+							break;
+						}
+						x = (e == 1) ? x = a : x = b;
+						putchar(x % 10 + '0');
+						if (e == 2)
+							putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
