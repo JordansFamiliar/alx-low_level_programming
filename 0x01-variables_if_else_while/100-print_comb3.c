4abaa8c6ce12c345a4bc36e8733d0adb5d5897a7
Jordan <measlybean@gmail.com>
2023-04-24 02:51:57 -0700

    Changes to file 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -15,20 +15,21 @@ int main(void)
 
 	/* init */
 	i = 1;
-	a = i;
+	a = 1;
 
 	while (i < 90)
 	{
-		putchar((i % 10 + '0'));
 		if (i % 10 == 9)
 		{
-			a = a + 11;
+			a += 11;
 			i = a;
+			putchar((i % 10 + '0'));
 			continue;
 		}
 
 		if (i < 89)
 		{
+			putchar((i % 10 + '0'));
 			putchar(',');
 			putchar(' ');
 		}
