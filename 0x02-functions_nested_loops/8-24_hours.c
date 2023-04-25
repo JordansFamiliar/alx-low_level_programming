db33f3f0fa59bfb38f2e8abd1b82eee1ce269f6e
Jordan <measlybean@gmail.com>
2023-04-25 14:57:03 -0700

    Changes to file

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,13 +9,13 @@ void jack_bauer(void)
 {
 	int a, b, c, d;
 
-	for (a = 0; a < 3; a++)
+	for (a = 0; a <= 2; a++)
 	{
-		for (b = 0; b < 10; b++)
+		for (b = 0; b <= 9; b++)
 		{
-			for (c = 0; c < 6; c++)
+			for (c = 0; c <= 5; c++)
 			{
-				for (d = 0; d < 10; d++)
+				for (d = 0; d <= 9; d++)
 				{
 					_putchar((a + '0'));
 					_putchar((b + '0'));
@@ -25,8 +25,8 @@ void jack_bauer(void)
 					_putchar('\n');
 				}
 			}
+			if (a == 2 && b == 3)
+				break;
 		}
-		if (a == 2 && b == 3 && c == 5 && d == 9)
-			break;
 	}
 }
