8594d6b8baf34d15226f653a431c567162153540
Jordan <measlybean@gmail.com>
2023-04-26 13:31:26 -0700

    changes to file

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -14,7 +14,7 @@ void print_remaining_days(int month, int day, int year)
 	int max_days = get_max_days(month);
 
 	if ((year % 4 == 0 && year % 100 != 0) ||
-	    (year % 400 == 0))
+	    (year % 400 == 0) || (year % 400 == 0 && year % 100 == 0))
 	{
 		if (month >= 2 && day >= 60)
 			day++;
