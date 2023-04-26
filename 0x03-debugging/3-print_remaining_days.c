6ac85efffd6415d3de6da7ad6330aa05779fef2c
Jordan <measlybean@gmail.com>
2023-04-26 15:17:15 -0700

    Changes to file

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -11,7 +11,6 @@
 */
 void print_remaining_days(int month, int day, int year)
 {
-	int max_days = get_max_days(month);
 
 	if ((year % 4 == 0 && year % 100 != 0) ||
 	    (year % 400 == 0) || (year % 400 == 0 && year % 100 == 0))
@@ -23,7 +22,7 @@ void print_remaining_days(int month, int day, int year)
 	}
 	else
 	{
-		if (day > max_days)
+		if (month == 2 && day == 60)
 		{
 			printf("Invalid date: %02d/%02d/%04d\n"
 			       , month, day - 31, year);
@@ -34,39 +33,4 @@ void print_remaining_days(int month, int day, int year)
 			printf("Remaining days: %d\n", 365 - day);
 		}
 	}
-}
-/**
-* get_max_days - calculates the maximum days for each month
-* @a: first argument
-* Return: max days
-*/
-int get_max_days(int a)
-{
-	int max_days, odd_days, even_days;
-
-	if (a > 2)
-	{
-		if (a % 2 == 0)
-		{
-			odd_days = (a / 2) * 31;
-			even_days = (a / 2) * 30;
-		}
-		if (a % 2 != 0)
-		{
-			even_days = ((a - 1) / 2) * 30;
-			odd_days = ((a - 1) / 2 + 1) * 31;
-		}
-		max_days = (odd_days + even_days) - 2;
-
-	}
-	if (a == 2)
-	{
-		max_days = 59;
-	}
-	if (a < 2)
-	{
-		max_days = 31;
-	}
-
-	return (max_days);
 }
