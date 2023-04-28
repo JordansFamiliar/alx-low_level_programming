cc2af2b6b2b81ef0dd29f07dca225f0b99db3f35
Jordan <measlybean@gmail.com>
2023-04-28 06:24:00 -0700

    Changes to file

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,7 +9,7 @@ int main(void)
 {
 	int n;
 
-	for (n = 1; n <= 100; n++)
+	for (n = 1; n < 100; n++)
 	{
 		if (n % 3 == 0 && n % 5 != 0)
 			printf("Fizz ");
@@ -20,6 +20,7 @@ int main(void)
 		else
 			printf("%d ", n);
 	}
+	printf("Buzz");
 	putchar('\n');
 
 	return (0);
