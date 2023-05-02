35c5b174abca8c1bffeb4a03ee606ab03dd18a65
JordansFamiliar <129204588+JordansFamiliar@users.noreply.github.com>
2023-05-02 23:11:46 +0200

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -21,14 +21,14 @@ void rev_string(char *s)
 	}
 	b = malloc(sizeof(char) * (i + 1));
 	b[i] = '\0';
-        i-- 
-        for (; i >= 0; i--)
+	i--; 
+	for (; i >= 0; i--)
 	{
 		b[j] = s[i];
-                j++;
+		j++;
 	}
-        s[j] = '\0';
-        j--;
+	s[j] = '\0';
+	j--;
 	for (; j >= 0; j--)
 	{
 		s[j] = b[j];
