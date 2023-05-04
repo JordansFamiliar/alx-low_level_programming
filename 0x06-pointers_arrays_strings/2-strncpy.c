e7a98af070f49345935ad86929526c4988b1656c
Jordan <measlybean@gmail.com>
2023-05-04 08:11:07 -0700

    Changes to file 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,10 +12,8 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0; i < n, i++)
+	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
-		if (src[i] == '\0')
-			break;
 		dest[i] = src[i];
 	}
 	for (; i < n; i++)
