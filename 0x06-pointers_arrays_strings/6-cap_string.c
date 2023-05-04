22008d250c8ae8b1199c625d72f74ce26060b442
Jordan <measlybean@gmail.com>
2023-05-04 15:57:10 -0700

    changes to file

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -19,6 +19,10 @@ char *cap_string(char *s)
 			if (s[i + 1] >= 97 && s[i + 1] <= 122)
 				s[i + 1] -= 32;
 		}
+		if (i == 0 && s[i + 1] >= 97 && s[i + 1] <= 122)
+		{
+			s[i + 1] -= 32;
+		}
 		i++;
 	}
 	return (s);
