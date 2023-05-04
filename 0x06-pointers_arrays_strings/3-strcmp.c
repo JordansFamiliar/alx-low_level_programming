c26d8e5ff84c2e1c0ff2969df5d30488c7d4e135
Jordan <measlybean@gmail.com>
2023-05-04 10:59:37 -0700

    Changes to file

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -13,7 +13,9 @@ int _strcmp(char *s1, char *s2)
 
 	for (i = 0; !(s1[i] == '\0' && s2[i] == '\0'); i++)
 	{
-		retval += s1[i] - s2[i];
+		retval = s1[i] - s2[i];
+		if (retval == 0 || retval < 0 || retval > 0)
+			break;
 	}
 	return (retval);
 }
