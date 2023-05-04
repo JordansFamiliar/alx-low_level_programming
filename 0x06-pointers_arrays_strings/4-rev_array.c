208225f65a210c4c33c76ad2ece032d2c2af6f10
Jordan <measlybean@gmail.com>
2023-05-04 11:59:41 -0700

    jj

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -8,18 +8,17 @@
 
 void reverse_array(int *a, int n)
 {
-	/*int *i = &a[n - 1];*/
+	int *i = &a[n - 1];*/
 
 	int j = 0;
-	/*
-	 *for (n - 1; n >= 0; n--)
-	 *{
-	 *	i[j] = a[n];
-	 *	j++;
-	 *}
-	 *for (; n <= j; n++)
-	 *{
-	 *	a[n] = i[n];
-	 *}
-	 */
+
+	 for (n - 1; n >= 0; n--)
+	 {
+	 	i[j] = a[n];
+	 	j++;
+	 }
+	 for (; n <= j; n++)
+	 {
+	 	a[n] = i[n];
+	 }
 }
