408d8d0f521d22881f566cc2dca2d5a0c1a146ff
Jordan <measlybean@gmail.com>
2023-05-13 06:30:24 -0700

    Changes to file

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -10,10 +10,18 @@
  */
 int main(int argc, char **argv)
 {
-	long int i, sum = 0;
+	long int i, j, sum = 0;
 
 	for (i = 1; i < argc; i++)
 	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] < 48 || argv[i][j] > 57)
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
 		if (atoi(argv[i]) > 0)
 			sum += atoi(argv[i]);
 		else if ((atoi(argv[i]) == 0 && strcmp(argv[i], "0") != 0)
