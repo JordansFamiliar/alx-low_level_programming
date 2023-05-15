c619646c52413cb9a4ae95ed2388e65bd0149ae4
Jordan <measlybean@gmail.com>
2023-05-15 09:29:16 -0700

    Changes to 3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -18,6 +18,8 @@ int **alloc_grid(int width, int height)
 		grid[i] = malloc(sizeof(int) * width);
 		if (grid[i] == NULL)
 		{
+			for (; j >= 0; j--)
+				free(grid[j]);
 			free(grid);
 			return (NULL);
 		}
