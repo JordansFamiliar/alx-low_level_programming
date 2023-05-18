1b42e777198cac21b3797bbb3f1ef6b1ba783381
Jordan <measlybean@gmail.com>
2023-05-18 11:43:26 -0700

    Changes to return

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -27,8 +27,8 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	}
 	if (ptr == NULL)
 	{
-		ptr = malloc(new_size);
-		return (ptr);
+		p = malloc(new_size);
+		return (p);
 	}
 	p = malloc(new_size);
 	if (p == NULL)
