609898528cabe2fb1e7be3e0f71b6e2582c07fea
Jordan <measlybean@gmail.com>
2023-05-20 07:29:38 -0700

    Added helper function for 5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,25 +1,31 @@
 #include "main.h"
 
 /**
+ * int_division - helper function
+ * @n: number
+ * @i: denominator
+ * Return: int
+ */
+int int_division(int n, int i)
+{
+	if (n / i == i && n % i == 0)
+		return (i);
+	if (n / i < i)
+		return (-1);
+	else
+		return (int_division(n, i + 1));
+}
+/**
  * _sqrt_recursion -  returns the natural square root of a number
  * @n: number
  * Return: int
  */
 int _sqrt_recursion(int n)
 {
-	int rt, i;
-
 	if (n == 0 || n == 1)
 		return (n);
 	if (n < 0)
 		return (-1);
-	rt = _sqrt_recursion(n - 1);
-	i = n - rt * rt;
-
-	if (i == 0)
-		return (rt);
-	if (i >= (2 * rt + 1))
-		return (rt + 1);
 	else
-		return (-1);
+		return (int_division(n, 1));
 }
