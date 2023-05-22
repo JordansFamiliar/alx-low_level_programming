6727e9113e0437738e7124a848600275141333d0
Jordan <measlybean@gmail.com>
2023-05-22 13:15:04 -0700

    Added files

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,35 @@
+#include "dog.h"
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * new_dog - a function that creates a new dog
+ * @name: name of new dog
+ * @age: age of new dog
+ * @owner: name of owner
+ * Return: dog_t *
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *d = malloc(sizeof(dog_t));
+
+	if (d == NULL)
+		return (d);
+	d -> name = malloc(strlen(name) + 1);
+	if (d -> name == NULL)
+	{
+		free(d);
+		return (d);
+	}
+	d -> owner = malloc(strlen(owner) + 1);
+	if (d -> owner == NULL)
+	{
+		free(d -> name);
+		free(d);
+		return (d);
+	}
+	strcpy(d -> name, name);
+	strcpy(d -> owner, owner);
+	d -> age = age;
+	return (d);
+}
---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - a function that frees dogs
+ * @d: pointer to dog
+ * Return: void
+ */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		if (d -> name != NULL)
+			free(d -> name);
+		if (d -> owner != NULL)
+			free(d -> owner);
+		free(d);
+	}
+}
