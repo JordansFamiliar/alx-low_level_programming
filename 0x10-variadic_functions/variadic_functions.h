f371d11148adf02efd814fab3a6ef8d35ab5f9a5
Jordan <measlybean@gmail.com>
2023-05-25 14:48:46 -0700

    changes to variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,10 +1,5 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
-typedef struct
-{
-	const char *c;
-	const char *t;
-} type;
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
