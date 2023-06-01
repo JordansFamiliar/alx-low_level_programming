184afd2b6d62e0ea061ed6f1832a745846350b28
Jordan <measlybean@gmail.com>
2023-06-01 11:57:44 -0700

    redone 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,24 +10,15 @@
 size_t print_list(const list_t *h)
 {
 	size_t num = 0;
-	if (h != NULL)
+
+	while (h != NULL)
 	{
-		if (h->next == NULL)
-		{
-			if (h->str == NULL)
-				printf("[0] (nil)\n");
-			else
-				printf("[%d] %s\n", h->len, h->str);
-			return (++num);
-		}
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
 		else
-		{
-			if (h->str == NULL)
-				printf("[0] (nil)\n");
-			else
-				printf("[%d] %s\n", h->len, h->str);
-			return (++num + print_list(h->next));
-		}
+			printf("[%d] %s\n", h->len, h->str);
+		++num;
+		h = h->next;
 	}
 	return (num);
 }
