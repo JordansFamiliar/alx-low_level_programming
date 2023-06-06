f5774f401c5ffd018bcacc692bcd254e846084dc
Jordan <measlybean@gmail.com>
2023-06-06 02:57:30 -0700

    Added check for a null pointer

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -11,7 +11,7 @@ void free_listint2(listint_t **head)
 	listPtr current, next;
 
 	current = *head;
-	while (current != NULL)
+	while (current != NULL && head != NULL)
 	{
 		next = current->next;
 		free(current);
